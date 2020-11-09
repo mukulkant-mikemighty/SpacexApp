a898118af1967796b88809edf6d5bff877a14994
mukulkant-mikemighty <mukulkantjohar@gmail.com>
2020-11-09 11:53:45 +0530

    config updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-app.use(express.static('./dist/SpaceX'));
+app.use(express.static('./dist/space-x'));
 
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/SpaceX/'});
+  res.sendFile('index.html', {root: 'dist/space-x/'});
 });
 app.listen(process.env.PORT || 8080);
