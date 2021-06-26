53d696f4ca755e7df9f9839e677bc65b78892fca
mukulkant-mikemighty <mukulkantjohar@gmail.com>
2021-06-26 16:47:40 +0530

    cc12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 
 @Component({
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SpaceX';
+
+  constructor(
+    private titleService: Title,
+    private meta: Meta,
+    
+    ) {}
+
+  ngOnInit() {
+  	this.titleService.setTitle(this.title);
+  	this.meta.addTag({name: 'keywords', content: 'Angular Project, Create Angular Project, Space X, Space, space'});
+    this.meta.addTag({name: 'description', content: 'Angular project'});
+    this.meta.addTag({name: 'author', content: 'Mukul Kant'});
+    var imgUrlPath= "../src/FFTB1.ico";
+    this.meta.updateTag({ name: 'og:image', content: imgUrlPath });
+  }
   
 }
