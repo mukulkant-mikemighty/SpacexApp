42f4d0c72bc36251222aea205cbeb94ce9ca893f
mukulkant-mikemighty <mukulkantjohar@gmail.com>
2022-01-03 14:56:06 +0530

    Update link.service.ts
    restore

--- src/app/link.service.ts
+++ src/app/link.service.ts
@@ -30,40 +30,40 @@ export class LinkService {
     
     if ( (!isNullOrUndefined (this.filter1)) && (isNullOrUndefined (this.launch1)) && (isNullOrUndefined (this.land_success1))){
      console.log("1");
-      return this.http.get('https://api.spaceXdata.com/v4/launches?&launch_year='+this.filter1);
+      return this.http.get('https://api.spaceXdata.com/v3/launches?&launch_year='+this.filter1);
       
     }
     else if( (!isNullOrUndefined (this.filter1)) && !(isNullOrUndefined(this.land_success1)) && (isNullOrUndefined (this.launch1))){
       console.log("2");
-      return this.http.get('https://api.spaceXdata.com/v4/launches?&launch_success=true&land_success='+this.land_success1+'&launch_year='+this.filter1);
+      return this.http.get('https://api.spaceXdata.com/v3/launches?&launch_success=true&land_success='+this.land_success1+'&launch_year='+this.filter1);
     }
     else if ((!isNullOrUndefined (this.filter1)) && !(isNullOrUndefined(this.launch1)) && (isNullOrUndefined (this.land_success1))){
       console.log("3");
-      return this.http.get('https://api.spaceXdata.com/v4/launches?&launch_success='+this.launch1+'&launch_year='+this.filter1);
+      return this.http.get('https://api.spaceXdata.com/v3/launches?&launch_success='+this.launch1+'&launch_year='+this.filter1);
     }
     else if( (isNullOrUndefined (this.filter1)) && !(isNullOrUndefined(this.land_success1)) ){
       console.log("4");
-      return this.http.get('https://api.spaceXdata.com/v4/launches?&launch_success=true&land_success='+this.land_success1);
+      return this.http.get('https://api.spaceXdata.com/v3/launches?&launch_success=true&land_success='+this.land_success1);
     }
     else if ((isNullOrUndefined (this.filter1)) && !(isNullOrUndefined(this.launch1)) && (isNullOrUndefined (this.land_success1))){
       console.log("5");
-      return this.http.get('https://api.spaceXdata.com/v4/launches?&launch_success='+this.launch1);
+      return this.http.get('https://api.spaceXdata.com/v3/launches?&launch_success='+this.launch1);
     }
     else if((isNullOrUndefined (this.filter1)) && (!isNullOrUndefined (this.launch1)) && (!isNullOrUndefined (this.land_success1))){
       console.log("6");
-      return this.http.get('https://api.spaceXdata.com/v4/launches?&launch_success=true&land_success=true');
+      return this.http.get('https://api.spaceXdata.com/v3/launches?&launch_success=true&land_success=true');
     }
     else if((isNullOrUndefined (this.filter1)) && (isNullOrUndefined (this.launch1)) && (isNullOrUndefined (this.land_success1))){
-      return this.http.get('https://api.spaceXdata.com/v4/launches?');
+      return this.http.get('https://api.spaceXdata.com/v3/launches?');
     }
     else{
       console.log("7");
-      console.log('https://api.spaceXdata.com/v4/launches?&launch_success='+this.launch1+'&land_success='+this.land_success1+'&launch_year='+this.filter1);
-      return this.http.get('https://api.spaceXdata.com/v4/launches?&launch_success='+this.launch1+'&land_success='+this.land_success1+'&launch_year='+this.filter1);
+      console.log('https://api.spaceXdata.com/v3/launches?&launch_success='+this.launch1+'&land_success='+this.land_success1+'&launch_year='+this.filter1);
+      return this.http.get('https://api.spaceXdata.com/v3/launches?&launch_success='+this.launch1+'&land_success='+this.land_success1+'&launch_year='+this.filter1);
     }
     
   }
   hit1() :Observable <any>{
-    return this.http.get('https://api.spaceXdata.com/v4/launches?');
+    return this.http.get('https://api.spaceXdata.com/v3/launches?');
   }
 }
