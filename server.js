1c23a55add78ae10b070c4faaf63419b77e48031
mukulkant-mikemighty <mukulkantjohar@gmail.com>
2024-11-19 21:43:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SpaceX'));
+app.use(express.static(__dirname + '/dist/SpacexApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/SpaceX/index.html'));
+res.sendFile(path.join(__dirname+'/dist/SpacexApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
